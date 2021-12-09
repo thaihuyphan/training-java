631a2a78439ce5c147c2cf228c1ad360e3dce52a
lethao <thaonguyenlephuong292@gmail.com>
2021-12-09 18:19:37 +0700

    update and delete feature - 1st commit

--- src/main/java/com/kegmil/example/pcbook/service/LaptopClient.java
+++ src/main/java/com/kegmil/example/pcbook/service/LaptopClient.java
@@ -1,16 +1,9 @@
 package com.kegmil.example.pcbook.service;
 
-import com.kegmil.example.pcbook.pb.CreateLaptopRequest;
-import com.kegmil.example.pcbook.pb.CreateLaptopResponse;
-import com.kegmil.example.pcbook.pb.Filter;
-import com.kegmil.example.pcbook.pb.LaptopServiceGrpc;
-import com.kegmil.example.pcbook.pb.Memory;
-import com.kegmil.example.pcbook.pb.SearchLaptopRequest;
-import com.kegmil.example.pcbook.pb.SearchLaptopResponse;
+import com.kegmil.example.pcbook.pb.*;
 import com.kegmil.example.pcbook.sample.Generator;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import com.kegmil.example.pcbook.pb.Laptop;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 
@@ -20,79 +13,126 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LaptopClient {
-  private static final Logger logger = Logger.getLogger(LaptopClient.class.getName());
+    private static final Logger logger = Logger.getLogger(LaptopClient.class.getName());
 
-  private final ManagedChannel channel;
-  private final LaptopServiceGrpc.LaptopServiceBlockingStub blockingStub;
+    private final ManagedChannel channel;
+    private final LaptopServiceGrpc.LaptopServiceBlockingStub blockingStub;
 
-  public LaptopClient(String host, int port) {
-    channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+    public LaptopClient(String host, int port) {
+        channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
 
-    blockingStub = LaptopServiceGrpc.newBlockingStub(channel);
-  }
-
-  public void shutdown() throws InterruptedException {
-    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-  }
-
-  public void createLaptop(Laptop laptop) {
-    CreateLaptopRequest request = CreateLaptopRequest.newBuilder().setLaptop(laptop).build();
-    CreateLaptopResponse response;
-
-    try {
-      response = blockingStub.createLaptop(request);
-    } catch (StatusRuntimeException e) {
-      if (e.getStatus().getCode() == Status.Code.ALREADY_EXISTS) {
-        logger.info("laptop ID already exists");
-        return;
-      }
-      logger.log(Level.SEVERE, "request failed: " + e.getMessage());
-      return;
-    } catch (Exception e) {
-      logger.log(Level.SEVERE, "request failed: " + e.getMessage());
-      return;
+        blockingStub = LaptopServiceGrpc.newBlockingStub(channel);
     }
 
-    logger.info("laptop created with ID: " + response.getId());
-  }
-
-  private void searchLaptop(Filter filter) {
-    logger.info("Search started");
-
-    SearchLaptopRequest request = SearchLaptopRequest.newBuilder().setFilter(filter).build();
-    Iterator<SearchLaptopResponse> responseIterator = blockingStub.searchLaptop(request);
-
-    while (responseIterator.hasNext()) {
-      SearchLaptopResponse response = responseIterator.next();
-      Laptop laptop = response.getLaptop();
-      logger.info("- found: " + laptop.getId());
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
 
-    logger.info("Search completed");
-  }
+    public void createLaptop(Laptop laptop) {
+        CreateLaptopRequest request = CreateLaptopRequest.newBuilder().setLaptop(laptop).build();
+        CreateLaptopResponse response;
 
-  public static void main(String[] args) throws InterruptedException {
-    LaptopClient client = new LaptopClient("0.0.0.0", 6565);
-    Generator generator = new Generator();
+        try {
+            response = blockingStub.createLaptop(request);
+        } catch (StatusRuntimeException e) {
+            if (e.getStatus().getCode() == Status.Code.ALREADY_EXISTS) {
+                logger.info("laptop ID already exists");
+                return;
+            }
+            logger.log(Level.SEVERE, "request failed: " + e.getMessage());
+            return;
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "request failed: " + e.getMessage());
+            return;
+        }
 
-    try {
-      for (int i = 0; i < 10; i++) {
-        Laptop laptop = generator.newLaptop();
-        client.createLaptop(laptop);
-      }
-
-      Memory minRam = Memory.newBuilder().setValue(8).setUnit(Memory.Unit.GIGABYTE).build();
-      Filter filter = Filter.newBuilder()
-          .setMaxPriceUsd(2000)
-          .setMinCpuCores(4)
-          .setMinCpuGhz(2.5)
-          .setMinRam(minRam)
-          .build();
-
-      client.searchLaptop(filter);
-
-    } finally {
-      client.shutdown();
+        logger.info("laptop created with ID: " + response.getId());
     }
-  }
+
+    private void searchLaptop(Filter filter) {
+        logger.info("Search started");
+
+        SearchLaptopRequest request = SearchLaptopRequest.newBuilder().setFilter(filter).build();
+        Iterator<SearchLaptopResponse> responseIterator = blockingStub.searchLaptop(request);
+
+        while (responseIterator.hasNext()) {
+            SearchLaptopResponse response = responseIterator.next();
+            Laptop laptop = response.getLaptop();
+            logger.info("- found: " + laptop.getId());
+        }
+
+        logger.info("Search completed");
+    }
+
+    private void updateLaptop(Laptop laptop) {
+        UpdateLaptopRequest request = UpdateLaptopRequest.newBuilder().setLaptop(laptop).build();
+        UpdateLaptopResponse response;
+
+        try {
+            response = blockingStub.updateLaptop(request);
+        } catch (StatusRuntimeException e){
+            if(e.getStatus().getCode() == Status.Code.NOT_FOUND) {
+                logger.info(e.getMessage());
+                return;
+            }
+            logger.log(Level.SEVERE, "request failed: " + e.getMessage());
+            return;
+        } catch (Exception e){
+            logger.log(Level.SEVERE, "request failed: " + e.getMessage());
+            return;
+        }
+
+        logger.info("Laptop with ID " + laptop.getId() + " updated");
+    }
+
+    private void deleteLaptop(String id) {
+        DeleteLaptopRequest request = DeleteLaptopRequest.newBuilder().setId(id).build();
+        DeleteLaptopResponse response;
+
+        try {
+            response = blockingStub.deleteLaptop(request);
+        } catch (StatusRuntimeException e){
+            if(e.getStatus().getCode() == Status.Code.NOT_FOUND){
+                logger.log(Level.SEVERE, e.getMessage());
+                return;
+            }
+            logger.log(Level.SEVERE, "request failed" + e.getMessage());
+            return;
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "request failed: " + e.getMessage());
+            return;
+        }
+        logger.info("Laptop with ID " + id + " deleted");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        LaptopClient client = new LaptopClient("0.0.0.0", 6565);
+        Generator generator = new Generator();
+
+        try {
+            for (int i = 0; i < 10; i++) {
+                Laptop laptop = generator.newLaptop();
+                client.createLaptop(laptop);
+            }
+
+            Memory minRam = Memory.newBuilder().setValue(8).setUnit(Memory.Unit.GIGABYTE).build();
+            Filter filter = Filter.newBuilder()
+                    .setMaxPriceUsd(2000)
+                    .setMinCpuCores(4)
+                    .setMinCpuGhz(2.5)
+                    .setMinRam(minRam)
+                    .build();
+
+            client.searchLaptop(filter);
+
+            Laptop laptop = generator.newLaptop();
+            client.createLaptop(laptop);
+            laptop = laptop.toBuilder().setName("This is a very beautiful laptop name").build();
+            client.updateLaptop(laptop);
+
+            client.deleteLaptop(laptop.getId());
+        } finally {
+            client.shutdown();
+        }
+    }
 }
--- src/main/java/com/kegmil/example/pcbook/service/LaptopService.java
+++ src/main/java/com/kegmil/example/pcbook/service/LaptopService.java
@@ -1,89 +1,139 @@
 package com.kegmil.example.pcbook.service;
 
-import com.kegmil.example.pcbook.pb.CreateLaptopRequest;
-import com.kegmil.example.pcbook.pb.CreateLaptopResponse;
-import com.kegmil.example.pcbook.pb.Filter;
-import com.kegmil.example.pcbook.pb.LaptopServiceGrpc;
-import com.kegmil.example.pcbook.pb.SearchLaptopRequest;
-import com.kegmil.example.pcbook.pb.SearchLaptopResponse;
+import com.kegmil.example.pcbook.pb.*;
 import io.grpc.Context;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
-import com.kegmil.example.pcbook.pb.Laptop;
 
 import java.util.UUID;
 import java.util.logging.Logger;
 
 public class LaptopService extends LaptopServiceGrpc.LaptopServiceImplBase {
 
-  private static final Logger logger = Logger.getLogger(LaptopService.class.getName());
+    private static final Logger logger = Logger.getLogger(LaptopService.class.getName());
 
-  private LaptopStore laptopStore;
+    private LaptopStore laptopStore;
 
-  public LaptopService(LaptopStore laptopStore) {
-    this.laptopStore = laptopStore;
-  }
-
-  @Override
-  public void createLaptop(CreateLaptopRequest request, StreamObserver<CreateLaptopResponse> responseObserver) {
-    Laptop laptop = request.getLaptop();
-    String id = laptop.getId();
-    logger.info("Get a create-laptop request with ID: " + id);
-
-    UUID uuid;
-    if (id.isEmpty()) {
-      uuid = UUID.randomUUID();
-    } else {
-      try {
-        uuid = UUID.fromString(id);
-      } catch (IllegalArgumentException e) {
-        responseObserver.onError(
-            Status.INVALID_ARGUMENT
-                .withDescription(e.getMessage())
-                .asRuntimeException());
-        return;
-      }
+    public LaptopService(LaptopStore laptopStore) {
+        this.laptopStore = laptopStore;
     }
 
-    if (Context.current().isCancelled()) {
-      logger.info("Request is cancelled");
-      responseObserver.onError(Status.CANCELLED.withDescription("Request is cancelled").asRuntimeException());
+    @Override
+    public void createLaptop(CreateLaptopRequest request, StreamObserver<CreateLaptopResponse> responseObserver) {
+        Laptop laptop = request.getLaptop();
+        String id = laptop.getId();
+        logger.info("Get a create-laptop request with ID: " + id);
+
+        UUID uuid;
+        if (id.isEmpty()) {
+            uuid = UUID.randomUUID();
+        } else {
+            try {
+                uuid = UUID.fromString(id);
+            } catch (IllegalArgumentException e) {
+                responseObserver.onError(
+                        Status.INVALID_ARGUMENT
+                                .withDescription(e.getMessage())
+                                .asRuntimeException());
+                return;
+            }
+        }
+
+        if (Context.current().isCancelled()) {
+            logger.info("Request is cancelled");
+            responseObserver.onError(Status.CANCELLED.withDescription("Request is cancelled").asRuntimeException());
+        }
+
+        Laptop other = laptop.toBuilder().setId(uuid.toString()).build();
+        try {
+            laptopStore.save(other);
+        } catch (AlreadyExistException e) {
+            responseObserver.onError(
+                    Status.ALREADY_EXISTS
+                            .withDescription(e.getMessage())
+                            .asRuntimeException());
+            return;
+        } catch (Exception e) {
+            responseObserver.onError(
+                    Status.INTERNAL.withDescription(e.getMessage()).asRuntimeException());
+            return;
+        }
+
+        CreateLaptopResponse response = CreateLaptopResponse.newBuilder().setId(other.getId()).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+        logger.info("Saved laptop with ID: " + other.getId());
     }
 
-    Laptop other = laptop.toBuilder().setId(uuid.toString()).build();
-    try {
-      laptopStore.save(other);
-    } catch (AlreadyExistException e) {
-      responseObserver.onError(
-          Status.ALREADY_EXISTS
-              .withDescription(e.getMessage())
-              .asRuntimeException());
-      return;
-    } catch (Exception e) {
-      responseObserver.onError(
-          Status.INTERNAL.withDescription(e.getMessage()).asRuntimeException());
-      return;
+    @Override
+    public void searchLaptop(SearchLaptopRequest request, StreamObserver<SearchLaptopResponse> responseObserver) {
+        Filter filter = request.getFilter();
+        logger.info("Got a search-laptop request with filter:\n" + filter);
+
+        laptopStore.search(filter, laptop -> {
+            logger.info("Found laptop with ID: " + laptop.getId());
+            SearchLaptopResponse response = SearchLaptopResponse.newBuilder().setLaptop(laptop).build();
+            responseObserver.onNext(response);
+        });
+
+        responseObserver.onCompleted();
+        logger.info("Search laptop completed");
     }
 
-    CreateLaptopResponse response = CreateLaptopResponse.newBuilder().setId(other.getId()).build();
-    responseObserver.onNext(response);
-    responseObserver.onCompleted();
+    @Override
+    public void updateLaptop(UpdateLaptopRequest request, StreamObserver<UpdateLaptopResponse> responseObserver) {
+        Laptop laptop = request.getLaptop();
+        String id = laptop.getId();
+        logger.info("Get update-laptop request for ID: " + id);
 
-    logger.info("Saved laptop with ID: " + other.getId());
-  }
+        try {
+            laptopStore.update(laptop);
+        } catch (NotExistException e) {
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription(e.getMessage())
+                            .asRuntimeException()
+            );
+            return;
+        } catch (Exception e) {
+            responseObserver.onError(
+                    Status.INTERNAL
+                            .withDescription(e.getMessage())
+                            .asRuntimeException()
+            );
+            return;
+        }
 
-  @Override
-  public void searchLaptop(SearchLaptopRequest request, StreamObserver<SearchLaptopResponse> responseObserver) {
-    Filter filter = request.getFilter();
-    logger.info("Got a search-laptop request with filter:\n" + filter);
+        UpdateLaptopResponse response = UpdateLaptopResponse.newBuilder().setId(id).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
 
-    laptopStore.search(filter, laptop -> {
-      logger.info("Found laptop with ID: " + laptop.getId());
-      SearchLaptopResponse response = SearchLaptopResponse.newBuilder().setLaptop(laptop).build();
-      responseObserver.onNext(response);
-    });
+        logger.info("Laptop with ID " + id + " updated");
+    }
 
-    responseObserver.onCompleted();
-    logger.info("Search laptop completed");
-  }
+    @Override
+    public void deleteLaptop(DeleteLaptopRequest request, StreamObserver<DeleteLaptopResponse> responseObserver) {
+        String id = request.getId();
+        logger.info("Get delete-laptop request with ID: " + id);
+
+        try {
+            laptopStore.delete(id);
+        } catch (NotExistException e) {
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription(e.getMessage())
+                            .asRuntimeException()
+            );
+            return;
+        } catch (Exception e) {
+            responseObserver.onError(
+                    Status.INTERNAL
+                            .withDescription(e.getMessage())
+                            .asRuntimeException()
+            );
+            return;
+        }
+        logger.info("Laptop with ID " + id + " deleted");
+    }
 }
